57706a43dfa5ddcb45b73ac6f59a928bc04c3ecc
luqmanMohammed <m.luqman077@gmail.com>
2022-03-30 21:35:40 +0530

    Remove obsolete ereventcrd module

--- ereventcrd/pkg/apis/eventsrunner.io/v1alpha1/doc.go
+++
@@ -1,6 +0,0 @@
-// Package v1alpha1 defines the v1alpha1 API under the eventsrunner.io group.
-// +k8s:deepcopy-gen=package
-// +k8s:defaulter-gen=TypeMeta
-// +groupName=eventsrunner.io
-package v1alpha1
-
