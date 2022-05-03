e1a442fca2cd57a79f733baaace72170be3a5fe8
luqmanMohammed <m.luqman077@gmail.com>
2022-05-04 00:30:40 +0530

    Update API types and introduce more states

--- crd/pkg/apis/eventsrunner.io/v1alpha1/types.go
+++ crd/pkg/apis/eventsrunner.io/v1alpha1/types.go
@@ -23,12 +23,12 @@ type Event struct {
 type EventType string
 
 const (
-	// ADDED is the type of the event when a new resource is added
-	ADDED EventType = "added"
-	// UPDATED is the type of the event when a resource is updated
-	UPDATED EventType = "updated"
-	// DELETED is the type of the event when a resource is deleted
-	DELETED EventType = "deleted"
+	// EventTypeAdded is the type of the event when a new resource is added
+	EventTypeAdded EventType = "added"
+	// EventTypeUpdated is the type of the event when a resource is updated
+	EventTypeUpdated EventType = "updated"
+	// EventTypeDeleted is the type of the event when a resource is deleted
+	EventTypeDeleted EventType = "deleted"
 )
 
 // EventSpec will be used to store the exact request recieved by the eventsrunner-api
@@ -39,10 +39,17 @@ type EventSpec struct {
 	// for each resource and will be used in the following combination to create the event
 	// <EventOriginID>-<ResourceID>-<Genrated Random>
 	// +optional
-	EventOriginID string    `json:"eventOriginID"`
-	ResourceID    string    `json:"resourceID"`
-	RuleID        string    `json:"ruleID"`
-	EventType     EventType `json:"eventType"`
+	EventOriginID string `json:"eventOriginID"`
+	// ResourceID along with the event origin id will be used to uniquely identify the resource
+	// This is required to prevent concurrent changes to the resource.
+	ResourceID string `json:"resourceID"`
+	// RuleID is the id of the rule that is being applied to the resource. Rule will be used to
+	// determine the runner to be used to process the event.
+	RuleID string `json:"ruleID"`
+	// EventType is the type of the change on the resource. Can be added, updated or deleted
+	// Used to configure a specific runner for a specific type of change.
+	EventType EventType `json:"eventType"`
+	// EventData contains any extra data that is passed to the runner.
 	// +optional
 	EventData []string `json:"eventData"`
 }
@@ -51,19 +58,42 @@ type EventSpec struct {
 type EventState string
 
 const (
-	// PENDING is the state of the event when is is ready to be processed
-	PENDING EventState = "pending"
-	// PROCESSING is the state of the event when it is being processed
-	PROCESSING EventState = "processing"
-	// PROCESSED is the state of the event when it is processed
-	PROCESSED EventState = "processed"
+	// EventStatePending is the state of the event is yet to be assigned a runner
+	EventStatePending EventState = "pending"
+	// EventStateWaiting is the state of the event where it is waiting for a dependent
+	// event to be processed before it can be processed
+	EventStateWaiting EventState = "waiting"
+	// EventStateAssigned is the state of the event is assigned to a runner
+	EventStateAssigned EventState = "assigned"
+	// EventStateProcessing is the state of the event is being processed by the runner
+	EventStateProcessing EventState = "processing"
+	// EventStateCompleted is the state of the event is processed by the runner successfully
+	EventStateCompleted EventState = "completed"
+	// EventStateFailed is the state of the event is processed by the runner failed
+	// will be retried if retries are available
+	EventStateFailed EventState = "failed"
+	// EventStateRetriesExpired is the state of the event is processed by the runner failed for
+	// the number of retries available
+	EventStateRetriesExpired EventState = "retriesExpired"
+	// EventStateDependentFailed is the state of the event when the dependent event failed
+	EventStateDependentFailed EventState = "dependentFailed"
 )
 
 // EventStatus will be used to store the current status of the event
 type EventStatus struct {
+	// State depicts the current state of the event
+	// +optional
 	State EventState `json:"status" default:"pending"`
+	// Retries depicts the number of retries that have been done for the event
 	// +optional
 	Retries int `json:"retries" default:"0"`
+	// RuleBindingName depicts the name of the rule binding that is
+	// associated with the event
+	// +optional
+	RuleBindingName string `json:"ruleBindingName"`
+	// RunnerName depicts the name of the runner that is associated with the event
+	// +optional
+	RunnerName string `json:"runnerName"`
 }
 
 // EventList is a list of EREvents
@@ -93,6 +123,37 @@ type RunnerList struct {
 	Items           []Runner `json:"items"`
 }
 
+// RunnerOveride defines overrides for the runner spec at the binding level.
+// This allows use of a single runner for multiple bindings with slight
+// variations in the spec.
+type RunnerOveride struct {
+	// Override the service account in the runner spec
+	// +optional
+	ServiceAccount string `json:"serviceAccount"`
+	// Override container spec in the runner spec.
+	// Map of container name to container overide spec
+	// +optional
+	Containers map[string]RunnerContainerOveride `json:"containers"`
+}
+
+// RunnerContainerOveride defines overrides for the runner container in the
+// runner spec.
+type RunnerContainerOveride struct {
+	// Override the environment variables in the container spec
+	// If the env name matches the value will be replaced
+	// +optional
+	Env []v1.EnvVar `json:"env,omitempty"`
+	// Override the command in the container spec
+	// +optional
+	Command []string `json:"command,omitempty"`
+	// Override the args in the container spec
+	// +optional
+	Args []string `json:"args,omitempty"`
+	// Override the volume mounts in the container spec
+	// +optional
+	Image string `json:"image,omitempty"`
+}
+
 // RunnerBinding binds a runner to one or more rules stating this runner
 // should picked for this rule or rules.
 // Runner level configuration can be overidden here, but it is recommended to
@@ -102,10 +163,19 @@ type RunnerList struct {
 type RunnerBinding struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Runner            string `json:"runner"`
+	// Name of the runner which is to be picked for this binding
+	Runner string `json:"runner"`
+	// Overrides for the runner spec at the binding level
 	// +optional
-	Overides v1.PodSpec `json:"overides"`
-	Rules    []string   `json:"rules"`
+	Overides *RunnerOveride `json:"overides"`
+	// Rules that this binding is associated with.
+	// Rules must be associated with a single binding, if multiple bindings
+	// are found the first one will be picked which can be unpredictable.
+	// Rules can associated as follows:
+	// 1. - rule1 - Runner to be picked for event types for this rule
+	// 2. - added:rule1 - Runner to be picked for added event type of this rule
+	// +optional
+	Rules []string `json:"rules"`
 }
 
 // RunnerBindingList is a list of RunnerBindings
