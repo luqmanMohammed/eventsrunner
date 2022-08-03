612e817f3b4a8b5b675b03c2c6bca494cdc80753
luqmanMohammed <m.luqman077@gmail.com>
2022-08-03 23:38:28 +0530

    Add basic jobs handling

--- controllers/event_controller.go
+++ controllers/event_controller.go
@@ -14,19 +14,32 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/*
+
+New Event
+-> Update RunnerName if not set
+-> Check if has any depencies
+-> If no dependencies, shedule a job
+-> If dependencies, wait for them to be completed
+*/
+
 package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	eventsrunneriov1alpha1 "github.com/luqmanMohammed/eventsrunner/api/v1alpha1"
 	runner "github.com/luqmanMohammed/eventsrunner/runner"
 	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EventReconciler reconciles a Event object
@@ -50,6 +63,7 @@ type EventReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
 func (r *EventReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	fmt.Println(req.NamespacedName.String())
 	logger := log.FromContext(ctx)
 	var event eventsrunneriov1alpha1.Event
 	if err := r.Get(ctx, req.NamespacedName, &event); err != nil {
@@ -70,6 +84,41 @@ func (r *EventReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 			logger.Error(err, "unable to update event")
 			return ctrl.Result{}, err
 		}
+		return ctrl.Result{}, nil
+	}
+	// Check if job is already created
+	var job batchv1.Job
+	if err := r.Get(ctx, client.ObjectKey{Namespace: event.Namespace, Name: fmt.Sprintf("job-%s", event.Name)}, &job); err != nil {
+		if event.Spec.DependsOn == "" {
+			runner, err := r.RunnerManager.GetRunner(ctx, event.Spec.RunnerName)
+			if err != nil {
+				logger.Error(err, "unable to get runner")
+				return ctrl.Result{}, err
+			}
+			createJob := batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("job-%s", event.Name),
+					Namespace: event.Namespace,
+				},
+				Spec: batchv1.JobSpec{
+					Template: corev1.PodTemplateSpec(runner.Spec),
+				},
+			}
+			if err := controllerutil.SetControllerReference(&event, &createJob, r.Scheme); err != nil {
+				logger.Error(err, "unable to set controller reference")
+				return ctrl.Result{}, err
+			}
+			if err := r.Create(ctx, &createJob); err != nil {
+				logger.Error(err, "unable to create job")
+				return ctrl.Result{}, err
+			}
+		} else {
+			return ctrl.Result{}, nil
+		}
+	} else {
+		fmt.Println("Job already created", job.Name, job.Status.Succeeded)
+		logger.V(1).Info("Job already created")
+		return ctrl.Result{}, nil
 	}
 	return ctrl.Result{}, nil
 }
