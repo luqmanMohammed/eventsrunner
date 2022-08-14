57d613941ea76febff6ae34c7feec78d2c09c427
luqmanMohammed <m.luqman077@gmail.com>
2022-08-14 19:48:13 +0530

    Starndadize list options for helpers for single change

--- controller/internal/helpers/composite_helper.go
+++ controller/internal/helpers/composite_helper.go
@@ -8,19 +8,34 @@ import (
 type CompositeHelper struct {
 	runnerHelper
 	eventsHelper
+	ruleHelper
+	jobHelper
 }
 
 // NewCompositeHelper returns a new composite handler
-func NewCompositeHelper(controllerLabel, controllerNamespace string, client client.Client) CompositeHelper {
+func NewCompositeHelper(controllerLabel, controllerNamespace string, k8sClient client.Client) CompositeHelper {
+	listOptions := []client.ListOption{
+		client.InNamespace(controllerNamespace),
+		client.HasLabels{controllerLabel},
+	}
 	return CompositeHelper{
 		runnerHelper: runnerHelper{
-			client:          client,
-			runnerNamespace: controllerNamespace,
-			controllerLabel: controllerLabel,
-		},
-		eventsHelper: eventsHelper{
-			client:              client,
+			client:              k8sClient,
+			listOptions:         listOptions,
 			controllerNamespace: controllerNamespace,
 		},
+		eventsHelper: eventsHelper{
+			client:      k8sClient,
+			listOptions: listOptions,
+		},
+		ruleHelper: ruleHelper{
+			client:      k8sClient,
+			listOptions: listOptions,
+		},
+		jobHelper: jobHelper{
+			client:      k8sClient,
+			listOptions: listOptions,
+		},
 	}
+
 }
